f8895740f5e91766dbe5cb2b80a4bb633f91e91c
Sharon Umute <sharonumute@gmail.com>
2025-02-10 21:11:48 -0800

    StyledButton: Added Bright Blue Button Variant

--- src/assets/images/buttons/index.ts
+++ src/assets/images/buttons/index.ts
@@ -5,10 +5,17 @@ import blueDefaultButton from './BlueDefaultButton/button_blue_default.png';
 
 import purpleSquareButton from './BrightPurpleSquareButton/button_purple_square.png';
 
+import brightBlueButton_left from './BrightBlueButton/bright_blue_button_left.png';
+import brightBlueButton_middle from './BrightBlueButton/bright_blue_button_middle.png';
+import brightBlueButton_right from './BrightBlueButton/bright_blue_button_right.png';
+
 export {
     blueDefaultButton_left,
     blueDefaultButton_middle,
     blueDefaultButton_right,
     blueDefaultButton,
     purpleSquareButton,
+    brightBlueButton_left,
+    brightBlueButton_middle,
+    brightBlueButton_right,
 };
--- src/components/shared/StyledButton.tsx
+++ src/components/shared/StyledButton.tsx
@@ -1,25 +1,48 @@
-import { blueDefaultButton_left, blueDefaultButton_middle, blueDefaultButton_right } from '@/assets/images/buttons';
+import {
+    blueDefaultButton_left,
+    blueDefaultButton_middle,
+    blueDefaultButton_right,
+    brightBlueButton_left,
+    brightBlueButton_middle,
+    brightBlueButton_right,
+} from '@/assets/images/buttons';
 import './shared.css';
+
+type StyledButtonVariant = 'BrightBlueButton' | 'BlueButtonDefault';
 
 type StyledButtonProps = {
     label: string;
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
     className?: string;
+    variant?: StyledButtonVariant;
 };
 
-export const StyledButton = (props: StyledButtonProps) => {
-    const className =
+export const StyledButton = ({ label, onClick, className = '', variant = 'BlueButtonDefault' }: StyledButtonProps) => {
+    const aggClassName =
         'w-full relative inline-flex items-center justify-center transition-all bg-transparent border-0 p-0 duration-75 hover:scale-105 active:scale-95 m-0 ' +
-        props.className;
+        className;
+
+    const buttonParts = [blueDefaultButton_left, blueDefaultButton_middle, blueDefaultButton_right];
+
+    switch (variant) {
+        case 'BrightBlueButton':
+            buttonParts[0] = brightBlueButton_left;
+            buttonParts[1] = brightBlueButton_middle;
+            buttonParts[2] = brightBlueButton_right;
+            break;
+        case 'BlueButtonDefault':
+        default:
+            break;
+    }
 
     return (
-        <button onClick={props.onClick} className={className}>
+        <button onClick={onClick} className={aggClassName}>
             <div className="w-full relative h-16 flex items-stretch cursor-pointer group">
-                <img src={blueDefaultButton_left} className="rounded-l-lg flex-shrink-0 relative z-10" />
-                <img src={blueDefaultButton_middle} className="flex-grow -mx-018" />
-                <img src={blueDefaultButton_right} className="rounded-r-lg flex-shrink-0 relative z-10" />
+                <img src={buttonParts[0]} className="rounded-l-lg flex-shrink-0 relative z-10" />
+                <img src={buttonParts[1]} className="flex-grow -mx-018" />
+                <img src={buttonParts[2]} className="rounded-r-lg flex-shrink-0 relative z-10" />
                 <span className="absolute inset-0 flex items-center justify-center text-white drop-shadow cookie-run-font font-bold text-3xl text-outline">
-                    {props.label}
+                    {label}
                 </span>
             </div>
         </button>
